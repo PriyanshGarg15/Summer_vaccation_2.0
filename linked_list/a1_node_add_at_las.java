dd3009667876178dffe2939d9f92c58a99bc827a
PriyanshGarg15 <116974262+PriyanshGarg15@users.noreply.github.com>
2024-06-11 21:49:49 +0530

    Create a1_node_add_at_las.java

---
+++ linked_list/a1_node_add_at_las.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class a1_node_add_at_last {
+    public static class Node {
+        int data;
+        Node next;
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static class LinkedList {
+        Node head;
+        Node tail;
+        int size;
+
+        void addLast(int val) {
+            Node temp = new Node(val);
+            if (size == 0) {
+                head = tail = temp;
+            } else {
+                tail.next = temp;
+                tail = temp;
+            }
+            size++;
+        }
+
+        void display() {
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+
+        void printSize() {
+            System.out.println(size);
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.display(); 
+        list.printSize(); 
+    }
+}
